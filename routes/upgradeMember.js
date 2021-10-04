706a7a041ee0f4ec3d30d175f5695875ca6c3b31
Paula Leal <paula61395@gmail.com>
2021-10-04 17:30:49 -0400

    added forms to to upgrade member status page

--- routes/upgradeMember.js
+++ routes/upgradeMember.js
@@ -3,7 +3,7 @@ var upgradeMemberRouter = express.Router();
 
 /* GET users listing. */
 upgradeMemberRouter.get('/', function (req, res, next) {
-    res.render('upgradeMember', { title: 'Upgrade to Member Status' });
+    res.render('upgradeMember', { title: 'Upgrade to Member Status', user: req.user });
 });
 
 module.exports = upgradeMemberRouter;
