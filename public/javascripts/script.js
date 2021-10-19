fc328b4aeb64bf4e048f05d2915e4c0e4692fbc9
Paula Leal <paula61395@gmail.com>
2021-10-19 17:32:23 -0400

    added hamburger menu

---
+++ public/javascripts/script.js
@@ -0,0 +1,16 @@
+"use strict"
+
+let hamburgerMenu = document.querySelector('.hamburger-menu');
+let exit = document.querySelector('.exit');
+
+hamburgerMenu.addEventListener('click', function () {
+    document.querySelector('.exit').style.display = 'block';
+    document.querySelector('.drop-down').style.display = 'block';
+    hamburgerMenu.style.display = 'none';
+});
+
+exit.addEventListener('click', function () {
+    document.querySelector('.exit').style.display = 'none';
+    document.querySelector('.drop-down').style.display = 'none';
+    hamburgerMenu.style.display = 'block';
+});
